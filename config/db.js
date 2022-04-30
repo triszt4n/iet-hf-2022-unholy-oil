eebf3c71d0d90a6fb0399a2a8d9997425003a0c2
Beni <bucsy.beni@gmail.com>
2022-04-30 20:21:01 +0200

    fix connection string

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/EQN34F', { useNewUrlParser: true ,useUnifiedTopology: true});
+
+const connectionString = '';
 
 mongoose.connect(
-    'mongodb+srv://dbAdmin:a8iLIBk61WPK03GQ@developementcluster-zaeo2.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+    connectionString, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }
