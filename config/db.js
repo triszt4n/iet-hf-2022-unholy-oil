ae68f3190aed4e353dac0142e35ecc4e96193f7d
triszt4n <trisztanpiller@gmail.com>
2022-05-16 23:22:30 +0200

    Catch error of mongoose

--- config/db.js
+++ config/db.js
@@ -4,9 +4,11 @@ const { MONGODB_USER: user, MONGODB_PASSWORD: pw } = process.env
 
 const connectionString = `mongodb://${user}:${pw}@mongodb:27017`
 
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => console.error('CATCHED MONGOOSE', err))
 
 module.exports = mongoose
