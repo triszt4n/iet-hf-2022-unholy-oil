52fdad34a425c360f3445be38abbb5e95f15d18c
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-05-16 11:49:48 +0200

    Add login test fixture

--- test/e2e/login.js
+++ test/e2e/login.js
@@ -1,17 +1,31 @@
 import { Selector } from 'testcafe';
-
+import { ClientFunction } from 'testcafe';
 
 fixture`Example test`
     .page`localhost:3000`;
 
-test('My first test', async t =>{
+const getLocationPart = ClientFunction(locationPart => {
+    return window.location[locationPart];
+});
+
+test('Login and log out', async t =>{
+
      await t
-        .typeText('#developer-name', 'John Smith')
-        .click('#submit-button')
-        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+        .typeText('#username', process.env.ADMIN_USER)
+        .typeText('#password', process.env.ADMIN_PASSWORD)
+        .click('input[type=submit]')
 
-    const articleHeader = await Selector('.result-content').find('h1');
+        const btnLogout = Selector('#btn-logout')
+        let path = await getLocationPart('pathname')
 
-    // Obtain the text of the article header
-    let headerText = await articleHeader.innerText;
+        await t
+        .expect(path).eql('/bunkers','Login should redirect to bunkers page')
+        .expect(btnLogout.exists).ok('Logout button should exists, and be visible')
+
+        await t.click(btnLogout);
+
+        path = await getLocationPart('pathname');
+
+        await t.expect(path).eql('/', 'Logout should redirect to login page')
+
 })
