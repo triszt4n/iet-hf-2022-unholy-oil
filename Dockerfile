3977e7fa14fc36ae4491dd19e474d5470f449e0b
triszt4n <trisztanpiller@gmail.com>
2022-05-17 00:51:50 +0200

    Using authSource on mongo connect string

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM node:14
-COPY .env .env
+
+WORKDIR /src
+
+COPY .env .
 COPY package.json .
 COPY package-lock.json .
+
 RUN npm install
 COPY . .
+CMD npm run start:prod
--- config/db.js
+++ config/db.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose')
 
 const { MONGODB_USER: user, MONGODB_PASSWORD: pw } = process.env
 
-const connectionString = `mongodb://${user}:${pw}@0.0.0.0:27017`
+const connectionString = `mongodb://${user}:${pw}@0.0.0.0:27017/bunkermgr?authSource=admin`
 
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .catch((err) => console.error('CATCHED MONGOOSE', err))
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 module.exports = mongoose
