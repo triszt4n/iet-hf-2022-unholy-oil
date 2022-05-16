79933995fdb0ea28f70aed9c0d74431b6045bf86
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-05-16 18:31:24 +0200

    Re disable screenshots

--- .testcaferc.js
+++ .testcaferc.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: '.test.env' })
 module.exports = {
   skipJsErrors: true,
   hostname: 'localhost',
-  //disableScreenshots: true,
+  disableScreenshots: true,
   quarantineMode: true,
   src: './test/e2e/',
   appCommand: 'DOTENV_CONFIG_PATH=.test.env node index.js',
