7c5912ed187a7c0b40dfcd326b778e01b79675b5
triszt4n <trisztanpiller@gmail.com>
2022-05-16 22:47:27 +0200

    Changing database access to localhost

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const { MONGODB_USER: user, MONGODB_PASSWORD: pw } = process.env
 
-const connectionString = `mongodb://${user}:${pw}@mongodb:27017`
+const connectionString = `mongodb://${user}:${pw}@localhost:27017`
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
