af846c2b260dc5f469fe52bd16c70469cef45da8
triszt4n <trisztanpiller@gmail.com>
2022-05-17 02:11:00 +0200

    Use docker dns without port

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const { MONGODB_USER: user, MONGODB_PASSWORD: pw } = process.env
 
-const connectionString = `mongodb://${user}:${pw}@0.0.0.0:27017/bunkermgr?authSource=admin`
+const connectionString = `mongodb://${user}:${pw}@mongodb/bunkermgr?authSource=admin`
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
